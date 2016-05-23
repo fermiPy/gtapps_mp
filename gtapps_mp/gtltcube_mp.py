dfee0a338103fc22076c3eb4913544969a4a10e3
Matthew Dunseth Wood <mdwood137@gmail.com>
2016-05-23 10:07:01 -0700

    Switch to console_scripts mechanism.

--- gtapps_mp/gtltcube_mp.py
+++ gtapps_mp/gtltcube_mp.py
@@ -2,7 +2,7 @@
 
 from multiprocessing import Pool
 import numpy as np
-import astropy.io.fits as pyfits
+from gtapps_mp.utils import pyfits
 import tempfile
 import os
 import subprocess
--- setup.py
+++ setup.py
@@ -4,32 +4,17 @@ import sys
 import os
 from setuptools import setup, find_packages
 
-fermi_dir = os.environ.get("FERMI_DIR")
-
-if fermi_dir:
-    print "The Fermi Science tools seem to be set up."
-    print "Will install into "+fermi_dir+"."
-else:
-    print "The Fermi Science tools are not set up."
-    sys.exit()
-
 setup(name='gtapps_mp',
-      version='1.3',
+      version='1.4',
       description='Fermi LAT Multicore Scripts',
       author='Jeremy S. Perkins (FSSC)',
       author_email='fermihelp@milkyway.gsfc.nasa.gov',
       url='http://fermi.gsfc.nasa.gov/ssc/',
       packages=find_packages(exclude='tests'),
-      scripts=['scripts/gtdiffrsp_mp',
-               'scripts/gtexpmap_mp',
-               'scripts/gtltcube_mp',
-               'scripts/gttsmap_mp'],
-#      py_modules=['gtdiffrsp_mp',
-#                  'gtexpmap_mp',
-#                  'gtltcube_mp',
-#                  'gttsmap_mp'],
-#      data_files=[(fermi_dir+"/bin",['scripts/gtdiffrsp_mp',
-#                                     'scripts/gtexpmap_mp',
-#                                     'scripts/gtltcube_mp',
-#                                     'scripts/gttsmap_mp'])],
+      entry_points= {'console_scripts': [
+                      'gtdiffrsp_mp = gtapps_mp.gtdiffrsp_mp:cli',
+                      'gtexpmap_mp = gtapps_mp.gtexpmap_mp:cli',
+                      'gtltcube_mp = gtapps_mp.gtltcube_mp:cli',
+                      'gttsmap_mp = gtapps_mp.gttsmap_mp:cli',
+                      ]},
       )
